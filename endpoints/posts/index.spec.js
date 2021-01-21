42fd17b92554ee20e357b4e35afec035bb62e6cb
alfcastillo90 <cacr1990@gmail.com>
2021-01-21 11:30:03 -0300

    [ADD]: TDD middleware de autenticacion y de creacion de post

---
+++ endpoints/posts/index.spec.js
@@ -0,0 +1,51 @@
+const postHandlers = require('./index')
+
+describe('Enpoints', () => {
+    describe('posts', () => {
+        it('should  create', async () => {
+
+            const mockUsers = [
+                { id: 1 },
+                { id: 2 }
+            ];
+
+            const post = {
+                userId: 1,
+                id: 1,
+                title: "Titulo",
+                body: "Cuerpo del post"
+            }
+
+            const req = {
+                body: post
+            }
+
+            const res = {
+                status: jest.fn().mockReturnThis(),
+                send: jest.fn()
+            }
+
+            const axios = {
+                get: jest.fn().mockResolvedValue({ data:  mockUsers}),
+                post: jest.fn().mockResolvedValue({ data: { id: 1000 }})
+            }
+
+            await postHandlers({axios}).post(req, res)
+
+            expect(res.status.mock.calls).toEqual([[201]])
+            
+            expect(res.send.mock.calls).toEqual([
+                [ { id: 1000 } ]
+            ])
+
+            expect(axios.get.mock.calls).toEqual([
+                ['https://jsonplaceholder.typicode.com/users']   
+            ])
+
+            expect(axios.post.mock.calls).toEqual([
+               ['https://jsonplaceholder.typicode.com/posts', post] 
+            ])
+        });
+    })
+    
+})
